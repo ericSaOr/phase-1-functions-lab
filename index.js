21152fc2fe10fc5dbfb91c945597a5978f4bdaa9
Eric Sanchez <ericfsod@gmail.com>
2021-06-29 19:46:13 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+const headquarters = 42;
+const feet = 264;
+
+function distanceFromHqInBlocks (num){
+
+    if (num > 42){
+    return num - headquarters;
+    }else if (num < 42){
+        return headquarters - num;
+    }
+
+}
+
+function distanceFromHqInFeet (num){
+    return distanceFromHqInBlocks(num) * feet;
+    
+}
+
+function distanceTravelledInFeet (headquarters, num2){
+    if (num2 > headquarters){
+        return (num2 - headquarters)*feet;
+
+    }else{
+    return (headquarters - num2)*feet;
+    }
+}
+
+function calculatesFarePrice (headquarters, num2){
+    if (distanceTravelledInFeet(headquarters, num2) < 401){
+        return 0;
+    }else if(distanceTravelledInFeet(headquarters, num2) <= 2000){
+        return (distanceTravelledInFeet(headquarters, num2) - 400) * 0.02;
+    }else if (distanceTravelledInFeet(headquarters, num2) > 2500){
+        return "cannot travel that far"
+
+    }else if (distanceTravelledInFeet(headquarters, num2) > 2000){
+        return 25;
+    }
+
+}
